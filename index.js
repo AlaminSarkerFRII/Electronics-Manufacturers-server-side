a01827b348825eda8ff74a302ea917c29dbb0c63
AlaminSarkerFRII <alamin.education1997@gmail.com>
2022-05-22 20:59:36 +0600

    load all tools in ui

--- index.js
+++ index.js
@@ -1,6 +1,42 @@
 const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const cors = require("cors");
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
+
+//middle ware
+
+app.use(cors());
+app.use(express.json());
+
+// connect with mongodb
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.er99c.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    // tools create
+    const toolCollection = client.db("py_electrics").collection("tools");
+    // create api to load all tools
+    app.get("/tool", async (req, res) => {
+      const query = req.body;
+      const result = await toolCollection.find(query).toArray();
+      res.send(result);
+    });
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+// api create
 
 app.get("/", (req, res) => {
   res.send("Hello World");
