5859c64e8e78b9310cce44d08950792e0065dd9b
AlaminSarkerFRII <alamin.education1997@gmail.com>
2022-05-25 21:13:00 +0600

    implement jwt

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const jwt = require("jsonwebtoken");
 const cors = require("cors");
 require("dotenv").config();
 const app = express();
@@ -18,17 +19,48 @@ const client = new MongoClient(uri, {
   serverApi: ServerApiVersion.v1,
 });
 
+// verify JWT token start
+
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: "UnAuthorization Access" });
+  }
+  const token = authHeader.split(" ")[1]; // token er second elements
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+    if (err) {
+      res.status(403).send({ message: "Forbidden Access" });
+    }
+    req.decoded = decoded;
+    next(); // forwarding
+  });
+}
+
+// verify JWT token end
+
 async function run() {
   try {
     await client.connect();
     // tools create
     const toolCollection = client.db("py_electrics").collection("tools");
     const orderCollection = client.db("py_electrics").collection("orders");
+    const userCollection = client.db("py_electrics").collection("users");
     // create api to load all tools
     app.get("/tool", async (req, res) => {
       const query = req.body;
       const result = await toolCollection.find(query).toArray();
       res.send(result);
+    });
+
+    // get all orders in my orders page
+
+    app.get("/order", verifyJWT, async (req, res) => {
+      const query = {};
+
+      console.log("auth", authorization);
+      const orders = await orderCollection.find(query).toArray();
+      res.send(orders);
+      // console.log(orders);
     });
 
     //post user orders in database...
@@ -50,30 +82,50 @@ async function run() {
       // console.log(result);
     });
 
-    // post booking in database
-
-    // app.post("/booking", async (req, res) => {
-    //   const booking = req.body;
-    //   const query = {
-    //     treatment: booking.treatment,
-    //     date: booking.date,
-    //     patient: booking.patient,
-    //   };
-    //   const exists = await bookingCollection.findOne(query);
-    //   if (exists) {
-    //     return res.send({ success: false, booking: exists });
-    //   }
-    //   const result = await bookingCollection.insertOne(booking);
-    //   return res.send({ success: true, result });
-    // });
-
-    // find by id
+    // get user order by id
     app.get("/tool/:id", async (req, res) => {
       const id = req.params.id;
       // console.log(id);
       const tool = await toolCollection.findOne({ _id: ObjectId(id) });
       res.send(tool);
       // console.log(tool);
+    });
+
+    // delete
+
+    app.delete("/order/:id", async (req, res) => {
+      const id = req.params.id;
+      // console.log(id);
+      const tool = await orderCollection.deleteOne({ _id: ObjectId(id) });
+      res.send(tool);
+      // console.log(tool);
+    });
+
+    // get All from database
+
+    app.get("/user", async (req, res) => {
+      const query = {};
+      const users = await userCollection.find(query).toArray();
+      res.send(users);
+    });
+
+    // insert update user in db with jwt
+
+    app.put("/user/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const filter = { email: email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+      const result = await userCollection.updateOne(filter, updateDoc, options);
+      const token = jwt.sign(
+        { email: email },
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: "1h" }
+      );
+      res.send({ result, token });
     });
   } finally {
   }
